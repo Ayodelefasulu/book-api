4f9f144b3a4e4f163391271debde8eece9197fa7
ayodelefasulu <fasuluayodele@gmail.com>
2024-12-23 12:30:27 +0100

    deloyment with heroku

--- book_project/settings.py
+++ book_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-b372t4n)#&ds#c=ugthh=i_-cnah4dcxvl+1h$!pl(*f!%t==l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
